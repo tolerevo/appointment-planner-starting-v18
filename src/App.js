ac3d2f8600be1d6cddd8e049dfcabd46b6b6d355
tolerevo <tolerevo@hotmail.com>
2024-01-25 12:50:45 -0600

    add local states and passing functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
+import React, { useState } from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
@@ -9,23 +15,63 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [contacts, Setcontacts] = useState([]);
+  const [appointments, Setappointments] = useState([]);
 
   /*
   Implement functions to add data to
   contacts and appointments
   */
+  const addContact = (name, phone, email) => {
+    Setcontacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
 
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root/> }>
-      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
-    </Route>
-  ));
-  
-  return (
-    <RouterProvider router={router}/>
+  const addAppointment = (name, contact, date, time) => {
+    Setappointments([
+      ...appointments,
+      {
+        name: name,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
+        <Route
+          path={ROUTES.CONTACTS}
+          element={
+            <ContactsPage
+              contacts={contacts}
+              addContact={addContact}
+            /> /* Add props to ContactsPage */
+          }
+        />
+        <Route
+          path={ROUTES.APPOINTMENTS}
+          element={
+            <AppointmentsPage
+              appointments={appointments}
+              contacts={contacts}
+              addAppointment={addAppointment}
+            /> /* Add props to AppointmentsPage */
+          }
+        />
+      </Route>,
+    ),
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
